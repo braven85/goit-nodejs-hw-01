930c20a9996123d185d18a1506df4fe7b99db81e
Radek <radekie@wp.pl>
2022-04-24 18:17:08 +0200

    path update

--- contacts.js
+++ contacts.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 require("colors");
 
